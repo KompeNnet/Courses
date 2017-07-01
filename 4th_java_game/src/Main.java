1e14e6ed45f519fdbd5baed27b3bc9d362d7b6fa
Krompy <kompennet@gmail.com>
2017-07-01 22:11:33 +0300

    Got main logic. Need proof :)

--- 4th_java_game/src/Main.java
+++ 4th_java_game/src/Main.java
@@ -1,11 +1,11 @@
-import com.sun.deploy.util.SyncAccess;
-
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Krompy on 7/1/2017.
  */
+
 public class Main
 {
     private static List<String> items = new ArrayList<>();
@@ -15,11 +15,95 @@ public class Main
     {
         GetRules(args);
         PrintHelp();
-        while (!System.console().readLine().equalsIgnoreCase("quit"))
+        LetsPlay();
+    }
+
+    private static void LetsPlay()
+    {
+        String str = System.console().readLine();
+        while (!str.equalsIgnoreCase("quit"))
         {
-            // some game logic
-            System.out.print("lol\n");
+            int computer = ComputersChoise();
+            GetProof();
+            if (str.equalsIgnoreCase("help"))
+                PrintHelp();
+            else
+            {
+                int player = GetPlayersInput(str) - 1;
+                while (!isIndexExists(player))
+                {
+                    System.out.print("Doesn't exist. Please, try again:\n");
+                    PrintHelp();
+                    str = System.console().readLine();
+                    if (str.equalsIgnoreCase("quit")) return;
+                    else if (str.equalsIgnoreCase( "help")) PrintHelp();
+                    else player = GetPlayersInput(str);
+                }
+                int winner = isPlayerWin(computer, player);
+                PrintWinner(winner, computer, player);
+            }
+            str = System.console().readLine();
         }
+    }
+
+    private static void PrintWinner(int winner, int computer, int player)
+    {
+        if (winner == 1)
+        {
+            PrintProof();
+            System.console().printf("You're win!\nComputer: %s\nYou: %s\n\n", items.get(computer), items.get(player));
+        }
+        else if (winner == 0)
+        {
+            PrintProof();
+            System.console().printf("Oups, same chiose!\nComputer: %s\nYou: %s\n\n", items.get(computer), items.get(player));
+        }
+        else
+        {
+            PrintProof();
+            System.console().printf("Computer wins!\nComputer: %s\nYou: %s\n\n", items.get(computer), items.get(player));
+        }
+    }
+
+    private static void GetProof()
+    {
+        // proof here
+    }
+
+    private static void PrintProof()
+    {
+        // proof print here
+    }
+
+    private static int GetPlayersInput(String str)
+    {
+        try { return Integer.parseInt(str); }
+        catch (Exception e)
+        {
+            System.out.print("Please, try again:\n");
+            PrintHelp();
+            return GetPlayersInput(System.console().readLine());
+        }
+    }
+
+    private static int isPlayerWin(int computer, int player)
+    {
+        if (player < computer)
+            return ((computer + (player % 2)) % 2 == 1) ? 1 : -1;
+        if (player > computer)
+            return ((computer + (player % 2)) % 2 == 0) ? 1 : -1;
+        return 0;
+    }
+
+    private static boolean isIndexExists(int i)
+    {
+        return (i < items.size() && i >= 0);
+    }
+
+    private static int ComputersChoise()
+    {
+        SecureRandom random = new SecureRandom();
+        return random.nextInt(count);
     }
 
     private static void PrintHelp()
