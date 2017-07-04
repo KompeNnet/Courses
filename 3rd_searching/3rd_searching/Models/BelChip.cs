a9902a158e57b452a2fe1210a6cad1264eb5e4c9
Krompy <kompennet@gmail.com>
2017-07-04 22:32:02 +0300

    Got BelChip?

--- 3rd_searching/3rd_searching/Models/BelChip.cs
+++ 3rd_searching/3rd_searching/Models/BelChip.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,9 +8,31 @@ namespace _3rd_searching.Models
 {
     public class BelChip
     {
-        public List<Item> Parse(string request)
+        public List<Item> Parse(string query)
         {
-            return new List<Item>();
+            string request = @"http://belchip.by/search/?query=" + query;
+            HtmlDocument html = (new HtmlWeb()).Load(request);
+
+            var items = html.DocumentNode.SelectNodes("//*[@class='cat-item']");
+            List<Item> results = new List<Item>();
+
+            if (items != null)
+            {
+                foreach (HtmlNode item in items)
+                {
+                    Item exemplar = new Item()
+                    {
+                        ItemLink = @"http://belchip.by/" + item.SelectNodes(".//h3/a")[0].Attributes["href"].Value,
+                        Name = item.SelectNodes(".//h3/a")[0].InnerText,
+                        PictureLink = @"http://belchip.by/" + item.SelectNodes(".//a/img")?[1].Attributes["src"].Value,
+                        Price = item.SelectNodes(".//*[@class='denoPrice']")?[0].InnerText.Split(' ')[0] ?? "цена по запросу"
+                    };
+                    if (exemplar.Price == "цена по запросу") exemplar.Existance = "по запросу";
+                    else { exemplar.Existance = "на складе"; exemplar.Price += "BYN"; }
+                    results.Add(exemplar);
+                }
+            }
+            return results;
         }
     }
 }
--- 3rd_searching/3rd_searching/Models/Item.cs
+++ 3rd_searching/3rd_searching/Models/Item.cs
@@ -9,11 +9,10 @@ namespace _3rd_searching.Models
     public class Item
     {
         [Key]
-        public int ProductID { get; set; }
         public string Name { get; set; }
         public string ItemLink { get; set; }
         public string Price { get; set; }
         public string PictureLink { get; set; }
-        public string Existing { get; set; }
+        public string Existance { get; set; }
     }
 }
