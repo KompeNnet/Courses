d4de04845dfe755cfb1cfcacfe3c4a59fe0787bb
Krompy <kompennet@gmail.com>
2017-07-07 13:17:13 +0300

    Log in via Google

---
+++ 5th_registration/5th_registration/Models/ApplicationUsrerManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace _5th_registration.Models
+{
+    public class ApplicationUsrerManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUsrerManager(IUserStore<ApplicationUser> store) : base(store)
+        {
+
+        }
+
+        public static ApplicationUsrerManager Create(IdentityFactoryOptions<ApplicationUsrerManager> options, IOwinContext context)
+        {
+            ApplicationDbContext db = context.Get<ApplicationDbContext>();
+            ApplicationUsrerManager manager = new ApplicationUsrerManager(new UserStore<ApplicationUser>(db));
+            return manager;
+        }
+    }
+}
---
+++ 5th_registration/5th_registration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_5th_registration.Startup))]
+namespace _5th_registration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
