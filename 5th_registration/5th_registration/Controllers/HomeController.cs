f23bf5458689f4cb1969f684f4cc9d0a2cd8a28f
Krompy <kompennet@gmail.com>
2017-07-10 17:41:00 +0300

    Cleaned + authorization required

--- 5th_registration/5th_registration/Controllers/HomeController.cs
+++ 5th_registration/5th_registration/Controllers/HomeController.cs
@@ -2,25 +2,12 @@
 
 namespace _5th_registration.Controllers
 {
+    [Authorize]
     [RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
